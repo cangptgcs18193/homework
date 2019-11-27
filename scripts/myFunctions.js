4d1ca1b9bd2ea753e2050e23458b4f16c01ccfc6
Pham Trung Cang <cangptgcs18193@fpt.edu.vn>
2019-11-27 16:53:08 +0700

    First Commit

---
+++ scripts/myFunctions.js
@@ -0,0 +1,12 @@
+function tyAlert () {
+  alert ("Thank you for your interest in our fitness club. We will contact you soon.")
+}
+function burpees () {
+  window.open("scripts/burpees.html ","_blank", "width=610","height=360")
+}
+function plank () {
+  window.open("scripts/plank.html ","_blank", "width=610","height=360")
+}
+function mtnClimber () {
+  window.open("scripts/mtn.html ","_blank", "width=610","height=360")
+}
